faadc8807144ba7eab6a3b4afde41f0d4d0ebeaa
chrs-creyk <chrisacount117@gmail.com>
2023-03-02 23:06:53 -0500

    console v-0.1

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Console module"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
