002cacbc55036a1fbba9ba9c8f03137b0569ee20
jegomezV <jegomezvalencia@gmail.com>
2023-03-02 20:43:18 -0500

    console.py

--- models/base_model.py
+++ models/base_model.py
@@ -16,8 +16,7 @@ class BaseModel:
                 if key not in ["__class__"]:
                     setattr(self, key, value)
                 if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
