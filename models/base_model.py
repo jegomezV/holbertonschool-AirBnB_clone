1799a71079b8ebeeb5b61513e6c6ccc0f4af1738
jegomezV <jegomezvalencia@gmail.com>
2023-03-04 13:21:02 -0500

    file_storage remastered

--- models/base_model.py
+++ models/base_model.py
@@ -48,7 +48,8 @@ class BaseModel:
                 if key not in ["__class__"]:
                     setattr(self, key, value)
                 if key == "created_at" or  key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    form = '%Y-%m-%dT%H:%M:%S.%f'
+                    setattr(self, key, datetime.datetime.strptime(value, form))
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
